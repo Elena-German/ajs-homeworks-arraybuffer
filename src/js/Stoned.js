aa036abfeb738e7f7e285580934c188773ad1346
Elena German <web-lg@mail.ru>
2023-04-08 15:58:29 +0300

    переписан класс Stoned без #realAttack

--- src/js/Stoned.js
+++ src/js/Stoned.js
@@ -1,29 +1,29 @@
-/* eslint no-underscore-dangle: 0 */
 import Character from './Character';
 
 export default class Stoned extends Character {
   #attack = 0;
+
   #distance = 1;
+
   #stoned = false;
-  #realAttack = 0;
 
   get attack() {
-    if (this.#distance == 1) {
-      this.#attack = this.#realAttack;
+    if (this.#distance === 1) {
+      return this.#attack;
     }
     if (this.#distance > 1 && this.#distance < 11) {
-      this.#attack = (this.#realAttack * (110 - (this.#distance * 10))) / 100;
+      const attack = (this.#attack * (110 - (this.#distance * 10))) / 100;
       if (this.#stoned) {
-        this.#attack = this.#attack - (Math.log2(this.#distance) * 5);
+        return attack - (Math.log2(this.#distance) * 5);
       }
-    } else if (this.#distance >= 11) {
-      this.#attack = 0;
+      return attack;
+    } if (this.#distance >= 11) {
+      return 0;
     }
     return this.#attack;
   }
 
   set attack(attack) {
-    this.#realAttack = attack;
     this.#attack = attack;
   }
 
--- src/js/app.js
+++ src/js/app.js
@@ -5,3 +5,12 @@ import Magician from './Magician';
 // eslint-disable-next-line no-unused-vars
 import Daemon from './Daemon';
 // eslint-disable-next-line no-unused-vars
+
+/* const player = new Daemon('leo');
+console.log(`атака при создании нового игрока - ${player.attack}`); // должно быть 10
+player.distance = 2;
+console.log(`атака на вторую клетку - ${player.attack}`); // должно быть 9
+// player.distance = 1;
+player.levelUp();
+console.log(`атака после повышения уровня - ${player.attack}`); // должно быть 12
+*/
